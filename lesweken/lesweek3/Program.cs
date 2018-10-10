d7f6fbcf3864ca9f51ea613719ae2d86c0515d44
milangoossens <36628582+milangoossens@users.noreply.github.com>
2018-10-10 14:10:15 +0200

    lesweek3

---
+++ lesweken/lesweek3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace lesweek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rekenmachine
+            int deling(int c, int d)
+            {
+                return c / d;
+            }
+
+            int optellen(int c, int d)
+            {
+                return c + d;
+            }
+
+            int vermenigvuldigen(int c, int d)
+            {
+                return c * d;
+            }
+
+            int aftrekken(int c, int d)
+            {
+                return c - d;
+            }
+
+            int a = 5;
+            int b = 10; 
+
+            int resultaat1 = optellen(a,b);
+            int resultaat2 = aftrekken(a,b);
+            int resultaat3 = deling(a,b);
+            int resultaat4 = vermenigvuldigen(a,b);
+
+            Console.WriteLine(resultaat1);
+            Console.WriteLine(resultaat2);
+            Console.WriteLine(resultaat3);
+            Console.WriteLine(resultaat4);
+        }
+    }
+}
