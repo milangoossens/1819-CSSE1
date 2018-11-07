8b9d86a41020b4040fdc4fe57b2e9eff8cbb63b4
milangoossens <36628582+milangoossens@users.noreply.github.com>
2018-11-07 10:01:28 +0100

    opdrach2 aangepast

--- Opdrachten/Opdracht2/Program.cs
+++ Opdrachten/Opdracht2/Program.cs
@@ -1,29 +1,93 @@
 ﻿using System;
 
-namespace Opdracht2
+namespace Opdracht3
 {
     class Program
     {
-         public static int faculteit(int getal){
-                int resultaat = 1;
-                for(int i=1;i<=getal;i++){
-                    resultaat*= i;
+        // Functies oefening 1
+        public static void alletafels()
+        {
+            int resultaat= 0;
+            for (int i = 1; i < 11; i++)
+                {
+                    Console.WriteLine("De tafel van " + i + ": ");
+                    for (int j = 0; j < 11; j++)
+                    {
+                        resultaat = i * j;
+                        Console.WriteLine(resultaat);
+                    }
                 }
-                return resultaat;
             }
+
+        public static void gekozenTafel(int getal)
+        {
+            int resultaat = 0;
+            Console.WriteLine("De tafel van " + getal + ": ");
+            for (int i = 1; i < 11; i++)
+            {
+                resultaat = getal * i;
+                Console.WriteLine(resultaat);
+            }
+        }
+
+        // Functie oefening 2
+        public static int faculteit(int getal)
+        {
+            int resultaat = 1;
+            for(int i=1;i<=getal;i++)
+            {
+                resultaat*= i;
+            }
+            return resultaat;
+        }
+
+        // Functie oefening 3
+        public static void RijVanFibonacci()
+        {
+            int getal1, getal2, uitkomst;
+            getal1 = 0;
+            getal2 = 1;
+            for(int i = 0; i <= 45; i++)
+            {
+                if(i < 1)
+                    uitkomst = 0;
+                else
+                    uitkomst = getal1 + getal2;
+                getal1 = getal2;
+                getal2 = uitkomst;
+                Console.WriteLine(uitkomst);
+            }
+        }
+
         static void Main(string[] args)
         {
-            // Oefening 2
+            // oefening 1 
             
 
-            string n = "";
-            int a = 0;
-            Console.WriteLine("Van welk getal wil je de faculteit berekenen?");
-            n = Console.ReadLine();
-            a = Convert.ToInt16(n);
-            Console.WriteLine(faculteit(a));
+            alletafels();
 
-            // Oefening 3
+            string inputString = "";
+            int inputGetal = 0;
+            Console.WriteLine("Van welk getal wil je de tafel berekenen?");
+            inputString = Console.ReadLine();
+            inputGetal = Convert.ToInt16(inputString);
+            gekozenTafel(inputGetal);
+
+            // Oefening 2 
+
+            string inputString2 = "";
+            int inputGetal2 = 0;
+            Console.WriteLine("Van welk getal wil je de faculteit berekenen?");
+            inputString2 = Console.ReadLine();
+            inputGetal2 = Convert.ToInt16(inputString2);
+            Console.WriteLine("faculteit: ");
+            Console.WriteLine(faculteit(inputGetal2));
+
+            // Oefening 3 
+
+            Console.WriteLine("Rij van fibonacci: ");
+            RijVanFibonacci();
+
             
         }
     }
